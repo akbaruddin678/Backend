737f37ddfe435e9e43b6685818d6faf2bdad13e9
akbaruddin678 <akbaruddin678@gmail.com>
2025-05-09 01:37:24 +0500

    package added Database

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionResponse = await mongoose.connect(
-      "mongodb+srv://<akbaruddin678>:<Chitral123@>@cluster0.pe0mstx.mongodb.net/"
+      "mongodb+srv://akbaruddin:akbar1234@cluster0.qiwplqn.mongodb.net/NavttcProject"
+      // "mongodb+srv://akbaruddin:akbar1234@cluster0.eyhty.mongodb.net/NavttcProject?retryWrites=true&w=majority"
+      // "mongodb+srv://mongo:mongo@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     );
     console.log(
       `\n MONGODB Connected .... DB HOST : ${connectionResponse.connection.host}`
